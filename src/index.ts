70999128d882a15fcb9bf120aab29d6239e75908
dinn <dinn.ding@vechain.com>
2019-11-21 19:13:45 +0800

    get head by func

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,12 @@ import { Driver } from './driver';
 import { SimpleNet } from './simple-net';
 import { Framework } from '@vechain/connex-framework'
 const { getMetadata } = require('page-metadata-parser');
-
 // set connex
 let simpleNet = new SimpleNet()
-let driver = new Driver(simpleNet, window.genesis, window.initialHead)
+let initialHead = prompt('flutter_webview') as string;
+let driver = new Driver(simpleNet, window.genesis, JSON.parse(initialHead));
 Object.defineProperty(window, 'connex', { value: new Framework(driver), enumerable: true });
 delete window.genesis;
-delete window.initialHead;
 
 //get meta
 Object.defineProperty(window, '__getMetaData__', {
